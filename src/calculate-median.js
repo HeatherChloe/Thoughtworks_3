dd6a2e8310673e27e5a62a75cbc9ec762898e5ee
KhalilWang <hsgwpj@gmail.com>
2016-05-17 17:40:20 +0800

    Finished

---
+++ src/calculate-median.js
@@ -0,0 +1,23 @@
+function calculate_median(arr) {
+    var arr_even = [];
+    var i_even   = 0;
+    // get even num
+    for(var i = 0; i < arr.length; ++i){
+        if((i+1) % 2 == 0){
+            arr_even[i_even] = arr[i];
+            i_even++;
+        }
+    }
+
+    arr_even.sort();
+    var len = arr_even.length;
+    var rst = 0;
+    if(len % 2 == 0){
+        rst = (arr_even[len/2-1] + arr_even[len/2]) / 2;
+    }else{
+        rst = arr_even[len/2-0.5];
+    }
+    alert(rst);
+}
+
+module.exports = calculate_median;
